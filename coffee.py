47c895e116b11db87925e5b2ab3be802be2083ce
Grobe-Kartoffel <344briarwntling@gmail.com>
2025-10-12 22:31:41 -0400

    created initial settings class
    updated accuracy data to edit data in settings rather than in itself

--- coffee.py
+++ coffee.py
@@ -40,42 +40,13 @@ class Progress_Manager: # manages a progress bar for when Accuracy Data is readi
         pygame.draw.rect(surface,BORDERCOLOR,(x-border,y-border,w+border*2,h+border*2),border)
         pygame.draw.rect(surface,PROGRESSCOLOR,(x,y,w*self.Value,h))        
 class Accuracy_Data:
-    products = []       # product ID and description for each product
-    prodSupplies = []   # list of supplies needed for each product
-    prodSupplyAmt = []  # list of supply amounts needed for each product
-    prodPrices = []     # price of each product
-    probabilitySpaces = []  # 7x13 (day of week/hour of day) matrix containing the probability of an item ordered at that time on that day, being that product
-    supplies = []       # supply ID and description for each supply
-    supPrices = []      # price of each supply
+    probabilitySpaces = []  # 7x13 (day of week/hour of day) matrix containing the probability of an item ordered at that time on that day, being that product\
     def __init__(self):
-        self.products = []       # product ID and description for each product
-        self.prodSupplies = []   # list of supplies needed for each product
-        self.prodSupplyAmt = []  # list of supply amounts needed for each product
-        self.prodPrices = []     # price of each product
         self.probabilitySpaces = []  # 7x13 (day of week/hour of day) matrix containing the probability of an item ordered at that time on that day, being that product
-        self.supplies = []       # supply ID and description for each supply
-        self.supPrices = []      # price of each supply
     def __str__(self): # function is called when class object is placed in the print() function
         string = ""
         i = 0
-        while(i<len(self.products)):
-            string += f"ID:\t\t{self.products[i][0]}\nDesc:\t\t{self.products[i][1]}\n" # product ID and Description
-            j = 0
-            string += "Supplies:\t"                                                     # supplies
-            while(j<len(self.prodSupplies[i])):
-                string += f"[ID: {self.prodSupplies[i][j]}"
-                k = 0
-                while(k<len(self.supplies)):
-                    if(self.supplies[k][0]==self.prodSupplies[i][j]):
-                        string += f"; Desc: {self.supplies[k][1]}"
-                        break
-                    k += 1
-                string += f"; Amt: {self.prodSupplyAmt[i][j]}]"
-                j += 1
-                if(j<len(self.prodSupplies[i])):
-                    string += ", "
-            string += "\n"
-            string += f"Price:\t\t{self.prodPrices[i]}\n"                                   # price
+        while(i<len(self.probabilitySpaces)):
             j = 0
             string += "ProbabilitySpace:\n"
             while(j<len(self.probabilitySpaces[i])):                                    # probability space
@@ -93,7 +64,7 @@ class Accuracy_Data:
                 j += 1
             i += 1
         return string
-    def readSalesData(self,progressBar):
+    def readSalesData(self,settings,progressBar):
         progressBar.update(0)
         
         date = ""               # this is just to know when the day changes
@@ -179,18 +150,15 @@ class Accuracy_Data:
                 i += 1
             # we have read all the data from the line, now we have to store it
             prodIndex = 0   # I can't be bothered to sort the data right now, so we search the entire list to find which entry we are editing
-            while(prodIndex < len(self.products)):
-                if(self.products[prodIndex][0]==product): # we found a matching entry
+            while(prodIndex < len(settings.products)):
+                if(settings.products[prodIndex][0]==product): # we found a matching entry
                     # price and product details only change once
                     # quantity is the only value that will need updated with every entry
                     self.probabilitySpaces[prodIndex][weekDay][hour] += quantity
                     break
                 prodIndex += 1
-            if(prodIndex==len(self.products)): # we have a new product that we have not read before
-                self.products.append([product,desc])
-                self.prodSupplies.append([]) # this file does not have supply data, append an empty list as a placeholder
-                self.prodSupplyAmt.append([])
-                self.prodPrices.append(price)
+            if(prodIndex==len(settings.products)): # we have a new product that we have not read before
+                settings.addProd(product,desc,price)
                 self.probabilitySpaces.append( [[0.0]*15]*7 ) # 7 days in a week, 15 hours a day (6am - 9pm)
             # update progress bar
             progressBar.update( float(x+1)/float(len(lines)) )
@@ -201,17 +169,11 @@ class Accuracy_Data:
         # ratios of the popularities of chili mayan chocolate powder to other chocolate powders will be used to extrapolate chili mayan hot chocolate popularity from other hot chocolate drinks
         
         # initialize Chili Mayan Hot Chocolate Rg
-        self.products.append([62,"Drinking Chocolate|Hot chocolate|Chili Mayan Rg"])
-        self.prodSupplies.append([])
-        self.prodSupplyAmt.append([])
-        self.prodPrices.append(4.75)
+        settings.addProd(62,"Drinking Chocolate|Hot chocolate|Chili Mayan Rg",4.75)
         self.probabilitySpaces.append( [[0.0]*15]*7 )
         
         # initialize Chili Mayan Hot Chocolate Lg
-        self.products.append([63,"Drinking Chocolate|Hot chocolate|Chili Mayan Lg"])
-        self.prodSupplies.append([])
-        self.prodSupplyAmt.append([])
-        self.prodPrices.append(6.25)
+        settings.addProd(63,"Drinking Chocolate|Hot chocolate|Chili Mayan Lg",6.25)
         self.probabilitySpaces.append( [[0.0]*15]*7 )
         
         # vars needed for extrapolation
@@ -224,8 +186,8 @@ class Accuracy_Data:
         
         # find indexes
         prodIndex = 0
-        while(prodIndex < len(self.products)):
-            match(self.products[prodIndex][0]):
+        while(prodIndex < len(settings.products)):
+            match(settings.products[prodIndex][0]):
                 case 19: # dark chocolate powder
                     darkPowIndex = prodIndex
                 case 20: # organic chocolate powder
@@ -273,13 +235,12 @@ class Accuracy_Data:
         for day in range(7):
             for hour in range(15):
                 total = 0
-                for prod in range(len(self.products)):
+                for prod in range(len(settings.products)):
                     total += self.probabilitySpaces[prod][day][hour]
-                for prod in range(len(self.products)):
+                for prod in range(len(settings.products)):
                     self.probabilitySpaces[prod][day][hour] = float(self.probabilitySpaces[prod][day][hour]) / float(total)
-                
         return
-    def readSupplyData(self,progressBar):
+    def readSupplyData(self,settings,progressBar):
         progressBar.update(0)
         
         product = 0
@@ -337,35 +298,33 @@ class Accuracy_Data:
                 i += 1
             # we have read all the data from the line, now we have to store it
             supIndex = 0   # I can't be bothered to sort the data right now, so we search the entire list to find which entry we are editing
-            while(supIndex < len(self.supplies)):
-                if(self.supplies[supIndex][0]==supply): # we found a matching entry
+            while(supIndex < len(settings.supplies)):
+                if(settings.supplies[supIndex][0]==supply): # we found a matching entry
                     if(supplyAmt==1): # update price if we have an exact conversion on this line
-                        self.supPrices[supIndex] = supplyAmtPrice
+                        settings.setSupPrice(supIndex,supplyAmtPrice)
                     break
                 supIndex += 1
-            if(supIndex==len(self.supplies)): # we have a new supply that we have not read before
-                self.supplies.append([supply,""])
-                self.supPrices.append(0)
+            if(supIndex==len(settings.supplies)): # we have a new supply that we have not read before
+                settings.addSup(supply)
                 if(supplyAmt==1):
-                    self.supPrices[supIndex] = supplyAmtPrice
+                    settings.setSupPrice(supIndex,supplyAmtPrice)
             prodIndex = 0
-            while(prodIndex < len(self.products)): # set the desc of the supply
-                if(self.products[prodIndex][0]==supply):
-                    self.supplies[supIndex][1] = self.products[prodIndex][1]
+            while(prodIndex < len(settings.products)): # set the desc of the supply
+                if(settings.products[prodIndex][0]==supply):
+                    settings.setSupDesc(supIndex,settings.products[prodIndex][1])
                 if(supply==84):
-                    self.supplies[supIndex][1] = "Cup Sm"
+                    settings.setSupDesc(supIndex,"Cup Sm")
                 if(supply==85):
-                    self.supplies[supIndex][1] = "Cup Rg"
+                    settings.setSupDesc(supIndex,"Cup Rg")
                 if(supply==86):
-                    self.supplies[supIndex][1] = "Cup Lg"
+                    settings.setSupDesc(supIndex,"Cup Lg")
                 prodIndex += 1
             # we should have handled the supply entry
             # now we need to update the related product entry
             prodIndex = 0
-            while(prodIndex < len(self.products)):
-                if(self.products[prodIndex][0]==product):
-                    self.prodSupplies[prodIndex].append(supply)
-                    self.prodSupplyAmt[prodIndex].append(supplyAmt)
+            while(prodIndex < len(settings.products)):
+                if(settings.products[prodIndex][0]==product):
+                    settings.addProdSup(prodIndex,supply,supplyAmt)
                     break
                 prodIndex += 1
             # update progress bar
@@ -373,17 +332,80 @@ class Accuracy_Data:
         # always close files after using them
         salesFile.close()            
         return True
+    def createInventory(self,settings):
+        return
+class Settings:
+    products = []       # product ID and description for each product
+    prodSupplies = []   # list of supplies needed for each product
+    prodSupplyAmt = []  # list of supply amounts needed for each product
+    prodPrices = []     # price of each product
+    supplies = []       # supply ID and description for each supply
+    supPrices = []      # price of each supply
+    lock = threading.Lock()
+    def __init__(self):
+        self.products = []       # product ID and description for each product
+        self.prodSupplies = []   # list of supplies needed for each product
+        self.prodSupplyAmt = []  # list of supply amounts needed for each product
+        self.prodPrices = []     # price of each product
+        self.supplies = []       # supply ID and description for each supply
+        self.supPrices = []      # price of each supply
+        self.lock = threading.Lock()
+    def __str__(self): # function is called when class object is placed in the print() function
+        string = ""
+        i = 0
+        while(i<len(self.products)):
+            string += f"ID:\t\t{self.products[i][0]}\nDesc:\t\t{self.products[i][1]}\n" # product ID and Description
+            j = 0
+            string += "Supplies:\t"                                                     # supplies
+            while(j<len(self.prodSupplies[i])):
+                string += f"[ID: {self.prodSupplies[i][j]}"
+                k = 0
+                while(k<len(self.supplies)):
+                    if(self.supplies[k][0]==self.prodSupplies[i][j]):
+                        string += f"; Desc: {self.supplies[k][1]}"
+                        break
+                    k += 1
+                string += f"; Amt: {self.prodSupplyAmt[i][j]}]"
+                j += 1
+                if(j<len(self.prodSupplies[i])):
+                    string += ", "
+            string += "\n"
+            string += f"Price:\t\t{self.prodPrices[i]}\n\n\n"                                   # price
+            i += 1
+        return string
+    def addProd(self,ID,desc,price):
+        with self.lock:
+            self.products.append([ID,desc])
+            self.prodSupplies.append([])
+            self.prodSupplyAmt.append([])
+            self.prodPrices.append(price)
+    def addSup(self,ID):
+        with self.lock:
+            self.supplies.append([ID,""])
+            self.supPrices.append(0) 
+    def setSupPrice(self,ID,price):
+        with self.lock:
+            self.supPrices[ID] = price
+    def setSupDesc(self,ID,desc):
+        with self.lock:
+            self.supplies[ID][1] = desc
+        return
+    def addProdSup(self,prodID,supID,supAmt):
+        with self.lock:
+            self.prodSupplies[prodID].append(supID)
+            self.prodSupplyAmt[prodID].append(supAmt)            
 # -------- Main Program Loop -----------
 def main():                                             #every program should have a main function
                                                         #other functions go above main
     # local  variables
     data = Accuracy_Data()              # create class objects
     progressBar = Progress_Manager()
+    settings = Settings()
     
     dataState = 0 # indicate data has not started processing yet
     
-    salesDataThread = threading.Thread(target=data.readSalesData, args=(progressBar,))   # DO NOT INCLUDE PARENTHESIS ON TARGET FUNCTION    # ARGS MUST BE ITERABLE, INCLUDE EXTRA COMMA FOR ONLY 1 ARG
-    supplyDataThread = threading.Thread(target=data.readSupplyData, args=(progressBar,))
+    salesDataThread = threading.Thread(target=data.readSalesData, args=(settings,progressBar,))   # DO NOT INCLUDE PARENTHESIS ON TARGET FUNCTION    # ARGS MUST BE ITERABLE, INCLUDE EXTRA COMMA FOR ONLY 1 ARG
+    supplyDataThread = threading.Thread(target=data.readSupplyData, args=(settings,progressBar,))
     
     while (True):
         
@@ -418,6 +440,7 @@ def main():                                             #every program should ha
             supplyDataThread.join()
             dataState = 3 # indicate data is done processing
         if(dataState==3):
+            print(settings)
             print(data)
             return
         
