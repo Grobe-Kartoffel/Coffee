e7489bf916470892d3126ae48e3a4f7e8500394e
Grobe-Kartoffel <344briarwntling@gmail.com>
2025-11-12 16:36:12 -0500

    initial behavior for employee 1

--- coffee.py
+++ coffee.py
@@ -394,10 +394,14 @@ class Sim:
         self.employees = []
         self.customers = []
         self.objects = []
+        self.shelfSpace = [0,0]
+        self.prod_order = []
         self.demoStarted = False
         # image assets
+        # floorplan
         self.floorPlan = pygame.image.load("assets/floorplan.png").convert_alpha()
         self.floorPlan = pygame.transform.scale_by(self.floorPlan,SCALE)
+        # customers
         self.cust11 = pygame.image.load("assets/cust_1_1.png").convert_alpha()
         self.cust11 = pygame.transform.scale_by(self.cust11,SCALE)
         self.cust12 = pygame.image.load("assets/cust_1_2.png").convert_alpha()
@@ -430,7 +434,52 @@ class Sim:
         self.cust43 = pygame.transform.scale_by(self.cust43,SCALE)
         self.cust44 = pygame.image.load("assets/cust_4_4.png").convert_alpha()
         self.cust44 = pygame.transform.scale_by(self.cust44,SCALE)
+        # employees
+        self.emp11 = pygame.image.load("assets/emp_1_1.png").convert_alpha()
+        self.emp11 = pygame.transform.scale_by(self.emp11,SCALE)
+        self.emp12 = pygame.image.load("assets/emp_1_2.png").convert_alpha()
+        self.emp12 = pygame.transform.scale_by(self.emp12,SCALE)
+        self.emp13 = pygame.image.load("assets/emp_1_3.png").convert_alpha()
+        self.emp13 = pygame.transform.scale_by(self.emp13,SCALE)
+        self.emp14 = pygame.image.load("assets/emp_1_4.png").convert_alpha()
+        self.emp14 = pygame.transform.scale_by(self.emp14,SCALE)
+        self.emp21 = pygame.image.load("assets/emp_2_1.png").convert_alpha()
+        self.emp21 = pygame.transform.scale_by(self.emp21,SCALE)
+        self.emp22 = pygame.image.load("assets/emp_2_2.png").convert_alpha()
+        self.emp22 = pygame.transform.scale_by(self.emp22,SCALE)
+        self.emp23 = pygame.image.load("assets/emp_2_3.png").convert_alpha()
+        self.emp23 = pygame.transform.scale_by(self.emp23,SCALE)
+        self.emp24 = pygame.image.load("assets/emp_2_4.png").convert_alpha()
+        self.emp24 = pygame.transform.scale_by(self.emp24,SCALE)
+        self.emp31 = pygame.image.load("assets/emp_3_1.png").convert_alpha()
+        self.emp31 = pygame.transform.scale_by(self.emp31,SCALE)
+        self.emp32 = pygame.image.load("assets/emp_3_2.png").convert_alpha()
+        self.emp32 = pygame.transform.scale_by(self.emp32,SCALE)
+        self.emp33 = pygame.image.load("assets/emp_3_3.png").convert_alpha()
+        self.emp33 = pygame.transform.scale_by(self.emp33,SCALE)
+        self.emp34 = pygame.image.load("assets/emp_3_4.png").convert_alpha()
+        self.emp34 = pygame.transform.scale_by(self.emp34,SCALE)
+        self.emp41 = pygame.image.load("assets/emp_4_1.png").convert_alpha()
+        self.emp41 = pygame.transform.scale_by(self.emp41,SCALE)
+        self.emp42 = pygame.image.load("assets/emp_4_2.png").convert_alpha()
+        self.emp42 = pygame.transform.scale_by(self.emp42,SCALE)
+        self.emp43 = pygame.image.load("assets/emp_4_3.png").convert_alpha()
+        self.emp43 = pygame.transform.scale_by(self.emp43,SCALE)
+        self.emp44 = pygame.image.load("assets/emp_4_4.png").convert_alpha()
+        self.emp44 = pygame.transform.scale_by(self.emp44,SCALE)
     class Emp:
+        # static vars belonging to class, not one individual instance
+        # lists containing the orders that require each of the supply types so that emp1 knows where to get the proper supply item
+        sup_cof  = [1,2,3,4,5,6,7,8,9,10,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,87]    # coffee beans
+        sup_tea  = [11,12,13,14,15,16,17,18,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57]                # tea leaves
+        sup_coco = [19,20,21,58,59,60,61,62,63]                                                             # cocoa powders
+        sup_syr  = [64,65,66,67]                                                                            # syrups
+        sup_pas  = [69,70,71,72,73,74,75,76,77,78,79]                                                       # pastries
+        sup_mug  = [82]                                                                                     # mugs
+        sup_cup  = [83]                                                                                     # cups
+        sup_shr  = [81]                                                                                     # shirts
+        prod_wet = [22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,87]
+        prod_dry = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,64,65,66,67,69,70,71,72,73,74,75,76,77,78,79,81,82,83]
         def __init__(self,ID,loc,job,task):
             self.ID = ID
             self.loc = [loc[0],loc[1]]
@@ -439,6 +488,7 @@ class Sim:
             self.hand = None
             self.job = job
             self.task = task
+            self.order = 0
             self.patience = 3600
     class Cust:
         def __init__(self,ID,loc,order,task):
@@ -458,8 +508,12 @@ class Sim:
     def demoSim(self):
         if(not self.demoStarted):
             self.demoStarted = True
-            self.customers.append(self.Cust(random.randint(0,3),[-1,1],58,1)) # spawn a customer off screen, wanting a reg Dark Hot Chocolate, with the task of walking right forever (for testing purposes)
+            self.customers.append(self.Cust(random.randint(0,3),[-1,1],58,1)) # spawn a customer off screen, wanting a reg Dark Hot Chocolate, with the task of walking into the shop to order
+            self.employees.append(self.Emp(random.randint(0,3),[5,4],0,0)) # spawn an employee to take orders, with the task of waiting for a customer to order
+            self.employees.append(self.Emp(random.randint(0,3),[5,8],1,0)) # spawn an employee to give orders, with the task of waiting for an order to be made
+            self.employees.append(self.Emp(random.randint(0,3),[15,1],2,0)) # spawn an employee to clean tables, with the task of waiting for food to be left on the table
             return
+        # handle customers
         i = 0
         while(i<len(self.customers)):
             unit = self.customers[i]
@@ -523,9 +577,12 @@ class Sim:
                                 unit.offset[0] += 64
                                 unit.loc[0] -= 1
                 case 2: # ordering
-                    if(self.mouseXY[0]==7 and self.mouseXY[1]==4 and self.lftClkSt==1): # delete the customer to test things
-                        unit.task += 1
-                        unit.dir = 3
+                    if(self.mouseXY[0]==7 and self.mouseXY[1]==3 and self.lftClkSt==1):
+                        for e in self.employees:
+                            if(e.job==0 and e.task==0 and e.loc[0]==5 and e.loc[1]==4):
+                                unit.task += 1
+                                unit.dir = 3
+                                break
                 case 3: # walking to the pick-up line
                     if(unit.offset[0]<0):                                           # centering onto current tile
                         unit.offset[0] += 2
@@ -587,7 +644,7 @@ class Sim:
                                 unit.offset[0] += 64
                                 unit.loc[0] -= 1
                 case 5: # picking up order
-                    if(self.mouseXY[0]==7 and self.mouseXY[1]==8 and self.lftClkSt==1): # delete the customer to test things
+                    if(self.mouseXY[0]==5 and self.mouseXY[1]==7 and self.lftClkSt==1):
                         unit.task += 1
                         unit.dir = 3
                 case 6: # finding a seat
@@ -714,6 +771,147 @@ class Sim:
                             unit.offset[0] -= 64
                             unit.loc[0] += 1
             i += 1
+        # handle employees
+        i = 0
+        while(i<len(self.employees)):
+            unit = self.employees[i]
+            match(unit.job):
+                case 0: # take orders
+                    match(unit.task):
+                        case 0: # wait for a customer to order
+                            if(self.mouseXY[0]==7 and self.mouseXY[1]==3 and self.lftClkSt==1):
+                                unit.order = 0
+                                for c in self.customers:
+                                    if(c.task==3 and c.loc[0]==7 and c.loc[1]==4):
+                                        unit.order = c.order
+                                        break
+                                if(unit.order!=0): # make sure we actually got an order from the customer
+                                    unit.task += 1
+                                else:
+                                    i += 1
+                                    continue
+                                # face the correct way toward the supply item
+                                if unit.order in unit.sup_cof or unit.order in unit.sup_tea or unit.order in unit.sup_coco or unit.order in unit.sup_syr:
+                                    unit.dir = 2
+                                elif unit.order in unit.sup_pas:
+                                    unit.dir = 1
+                                else:
+                                    unit.dir = 3
+                        case 1: # grabbing a base ingredient
+                            if(unit.offset[0]>0):                   # centering
+                                unit.offset[0] -= 2
+                                if(unit.offset[0]==0):
+                                    if(unit.order in unit.sup_cof and unit.loc[0]==1) or (unit.order in unit.sup_tea and unit.loc[0]==2) or (unit.order in unit.sup_coco and unit.loc[0]==3) or (unit.order in unit.sup_syr and unit.loc[0]==4):
+                                        unit.dir = 1
+                            elif(unit.offset[1]<0 and unit.dir==3): # ^
+                                unit.offset[1] += 2
+                                if(unit.offset[1]==0):
+                                    if(unit.order in unit.sup_mug and unit.loc[1]==5) or (unit.order in unit.sup_cup and unit.loc[1]==6) or (unit.order in unit.sup_shr and unit.loc[1]==7):
+                                        unit.dir = 0
+                                        # put object in hand
+                                        unit.task += 1
+                            elif(unit.offset[1]>0 and unit.dir==1): # ^
+                                unit.offset[1] -= 2
+                                if(unit.offset[1]==0 and unit.loc[1]==3):
+                                    if(unit.order in unit.sup_cof and unit.loc[0]==1) or (unit.order in unit.sup_tea and unit.loc[0]==2) or (unit.order in unit.sup_coco and unit.loc[0]==3) or (unit.order in unit.sup_syr and unit.loc[0]==4) or (unit.order in unit.sup_pas and unit.loc[0]==5):
+                                        # put object in hand
+                                        unit.task += 1
+                            elif(unit.dir==2):                      # moving left
+                                unit.offset[0] -= 2
+                                if(unit.offset[0]<=32):
+                                    unit.offset[0] += 64
+                                    unit.loc[0] -= 1
+                            elif(unit.dir==1):                      # moving up
+                                unit.offset[1] -= 2
+                                if(unit.offset[1]<=-32):
+                                    unit.offset[1] += 64
+                                    unit.loc[1] -= 1
+                            elif(unit.dir==3):                      # moving down
+                                unit.offset[1] += 2
+                                if(unit.offset[1]>=32):
+                                    unit.offset[1] -= 64
+                                    unit.loc[1] += 1
+                        case 2: # placing a base ingredient on the back counter
+                            space = -1 # vertical offset from top of shelf, indicating where to place product
+                                       # if -1, no space, don't move
+                                       # otherwise, location is [0,4+space]
+                            if(unit.order in unit.prod_wet):
+                                space = self.shelfSpace[0]
+                            elif(unit.order in unit.prod_dry):
+                                if(self.shelfSpace[1]<0):
+                                    space = -1
+                                else:
+                                    space = self.shelfSpace[1]+3
+                            # try to move if there is room to place the product
+                            if(space<0):            # move on to next unit, this one cannot do anything this frame
+                                i += 1
+                                continue
+                            if(unit.dir<2):         # turn around and take 1 frame to do so
+                                unit.dir += 2
+                                i += 1
+                                continue
+                            if(unit.offset[1]<0):   # centering
+                                unit.offset[1] += 2
+                                if(unit.offset[1]==0):
+                                    if(unit.loc[1]==space+4):
+                                        unit.dir = 2
+                            elif(unit.offset[0]>0): # ^
+                                unit.offset[0] -= 2
+                                if(unit.offset[0]==0):
+                                    if(unit.loc[0]==1):     # we are in front of the space to place the product
+                                        # place product
+                                        # update shelf space
+                                        if(unit.order in unit.prod_wet):
+                                            self.shelfSpace[0] += 1
+                                            if(self.shelfSpace[0]>2):
+                                                self.shelfSpace[0] = -1
+                                        elif(unit.order in unit.prod_dry):
+                                            self.shelfSpace[1] += 1
+                                            if(self.shelfSpace[1]>1):
+                                                self.shelfSpace[1] = -1
+                                        # inform other employee of order
+                                        self.prod_order.append(unit.order)
+                                        unit.task += 1
+                                        unit.dir = 1
+                                        if(unit.loc[1]==4):
+                                            unit.dir = 0
+                                    elif(unit.loc[1]==3):   # double check that we are in front of the correct product location
+                                        if(unit.loc[1]!=space+4):
+                                            unit.dir = 3
+                            elif(unit.dir==2):      # walk left
+                                unit.offset[0] -= 2
+                                if(unit.offset[0]<=-32):
+                                    unit.offset[0] += 64
+                                    unit.loc[0] -= 1
+                            elif(unit.dir==3):      # walk down
+                                unit.offset[1] += 2
+                                if(unit.offset[1]>=32):
+                                    unit.offset[1] -= 64
+                                    unit.loc[1] += 1
+                        case 3: # return to take order
+                            if(unit.offset[1]>0):   # centering
+                                unit.offset[1] -= 2
+                                if(unit.offset[1]==0 and unit.loc[1]==4):
+                                    unit.dir = 0
+                            elif(unit.offset[0]<0): # ^
+                                unit.offset[0] += 2
+                                if(unit.offset[0]==0 and unit.loc[0]==5):
+                                    unit.task = 0
+                            elif(unit.dir==1):      # move up
+                                unit.offset[1] -= 2
+                                if(unit.offset[1]<=-32):
+                                    unit.offset[1] += 64
+                                    unit.loc[1] -= 1
+                            elif(unit.dir==0):      # move right
+                                unit.offset[0] += 2
+                                if(unit.offset[0]>=32):
+                                    unit.offset[0] -= 64
+                                    unit.loc[0] += 1
+                case 1: # give orders
+                    pass
+                case 2: # clean tables
+                    pass
+            i += 1
         # check for deleted customers
         i = 0
         while(i<len(self.customers)):
@@ -740,14 +938,19 @@ class Sim:
             self.lftClkSt = 0
             return
     def draw(self):
-        surface.blit(self.floorPlan, [0,0])                       # coffeeshop
+        # coffeeshop
+        surface.blit(self.floorPlan, [0,0])
+        #customers
         custs = [[self.cust11,self.cust12,self.cust13,self.cust14],[self.cust21,self.cust22,self.cust23,self.cust24],[self.cust31,self.cust32,self.cust33,self.cust34],[self.cust41,self.cust42,self.cust43,self.cust44]]
-        # draw customers newest to oldest so that oldest customers show up on top
-        i = len(self.customers)-1
+        i = len(self.customers)-1 # draw customers newest to oldest so that oldest customers show up on top
         while(i>-1):
             unit = self.customers[i]
             surface.blit(custs[unit.ID][unit.dir],[unit.loc[0]*16*SCALE+unit.offset[0],unit.loc[1]*16*SCALE+unit.offset[1]])
             i -= 1
+        # employees
+        emps = [[self.emp11,self.emp12,self.emp13,self.emp14],[self.emp21,self.emp22,self.emp23,self.emp24],[self.emp31,self.emp32,self.emp33,self.emp34],[self.emp41,self.emp42,self.emp43,self.emp44]]
+        for unit in self.employees:
+            surface.blit(emps[unit.ID][unit.dir],[unit.loc[0]*16*SCALE+unit.offset[0],unit.loc[1]*16*SCALE+unit.offset[1]])
 # -------- Main Program Loop -----------
 def main():                                             #every program should have a main function
                                                         #other functions go above main
