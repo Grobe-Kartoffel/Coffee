a9c36b07e855faaf218257139be78cc3df630af4
Grobe-Kartoffel <344briarwntling@gmail.com>
2025-12-08 14:38:07 -0500

    created basic simData functions

--- sim_data.py
+++ sim_data.py
@@ -1,46 +1,54 @@
 class SimData:
     def __init__(self):
         # Constructor. Define instance vars here.
-        self.supplyHistories = []
-        self.productHistories = []
+        self.supplies = []
+        self.products = []
         self.mouseXY = []
         self.hoverFrame = 20 # 0-20
     
-    class SupplyHIstory:
-        def __init__(self, ID, name, goalAmounts, orderedAmounts, usedAmounts):
+    class Supply:
+        def __init__(self, ID, name):
             self.ID = ID
             self.name = name
-            self.goalAmounts = goalAmounts
-            self.orderedAmounts = orderedAmounts
-            self.usedAmounts = usedAmounts
+            self.history = [] # [price,goalAmounts,orderedAmounts,usedAmounts]
     
-    class ProductHistory:
-        def __init__(self, ID, name, price, sales, supplyCost):
+    class Product:
+        def __init__(self, ID, name):
             self.ID = ID
             self.name = name
-            self.price = price
-            self.sales = sales
-            self.supplyCost = supplyCost
+            self.history = [] # [price,sales,supplyCost]
             # Revenue, cost, and profit can all be calculated with sales and supply cost.
     
     def addSupply(self, ID, name):
-        # TODO
-        pass
-    def addProduct(self, ID, name, price, cost):
-        # TODO
-        pass
-    def addSupplyHistory(self, ID, goal, ordered, used):
-        # TODO
-        pass
-    def addProductHistory(self, ID, sales):
-        # TODO
-        pass
+        self.supplies.append(self.Supply(ID, name))
+    def addProduct(self, ID, name):
+        self.products.append(self.Product(ID, name))
+    def addSupplyHistory(self, ID, price, goal, ordered, used):
+        for s in self.supplies:
+            if(s.ID==ID):
+                s.history.append([price,goal,ordered,used])
+                break
+    def addProductHistory(self, ID, price, sales, cost):
+        for p in self.products:
+            if(p.ID==ID):
+                p.history.append([price,sales,cost])
+                break
     def eraseHistory(self):
-        # TODO
-        pass
-    def storeInput(self):
-        # TODO
-        pass
+        for o in self.supplies:
+            del o.history
+        for o in self.products:
+            del o.history
+    def storeInputs(self,MouseXY,lftClk): # Takes user input and converts them into a better format for the Sim to use
+        self.mouseXY = MouseXY
+        if(self.lftClkSt==0 and lftClk): # mouse was clicked
+            self.lftClkSt = 1
+            return
+        if(self.lftClkSt==1 and lftClk): # mouse is held down
+            self.lftClkSt = 2
+            return
+        if(self.lftClkSt>0 and not lftClk): # mouse was unclicked
+            self.lftClkSt = 0
+            return
     def drawSupplyGraph(self, supplyID):
         # TODO
         # Draw empty graph if -1
